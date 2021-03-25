9deb3813a7bf4aae7823ef7de044f0f43004b8ab
Calvin Wu <calvinwu4@gmail.com>
2021-03-25 01:31:48 -0400

    Updated name and description

--- src/App.js
+++ src/App.js
@@ -8,25 +8,25 @@ import favicon48 from './favicon-48x48.png'
 import favicon128 from './favicon-128x128.png'
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          {this.props.isExt ? 
-            <img src={chrome.runtime.getURL("static/media/logo.svg")} className="App-logo" alt="logo" />
-          :
-            <img src={logo} className="App-logo" alt="logo" />
-          }
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           {this.props.isExt ? 
+//             <img src={chrome.runtime.getURL("static/media/logo.svg")} className="App-logo" alt="logo" />
+//           :
+//             <img src={logo} className="App-logo" alt="logo" />
+//           }
 
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+//           <h1 className="App-title">Welcome to React</h1>
+//         </header>
+//         <p className="App-intro">
+//           To get started, edit <code>src/App.js</code> and save to reload.
+//         </p>
+//       </div>
+//     );
+//   }
+// }
 
-export default App;
+// export default App;
